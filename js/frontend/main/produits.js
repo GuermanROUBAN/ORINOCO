c0e759c4448ee4418b0cbdf8faed282205a6029b
Guerman <guerman.rouban@gmail.com>
2021-05-03 12:37:31 +0200

    Affichage donnees sur produit.js depuis ID ok

--- js/frontend/main/produits.js
+++ js/frontend/main/produits.js
@@ -6,13 +6,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 
-console.log("id=",id);
+console.log("id=", id);
 
 //-----------------------------------------------------------------------------------------------
 
 const positionProduit = document.getElementById("selected_article");
 //console.log(positionProduit);
-
 
 let donnees = [];
 
@@ -20,49 +19,42 @@ let donnees = [];
 const fetchProduit = async () => {
 	donnees = await fetch(`http://localhost:3000/api/teddies/${id}`)
 		.then(res => res.json());
-	
+	// structureProduit(donnees);
 	//console.log(donnees); // ca m'affiche bien l'Api	
-	structureProduit(chargement);
+	positionProduit.innerHTML = structureProduit(donnees);
+
 };
 
-	
+
 //fetchProduit(); // ok on a bien recuperé l API
 const showProduit = async () => {
-	await fetchProduit();
+await fetchProduit();
 }
 showProduit();
 
-function structureProduit () {
+function structureProduit(parametre) {
 
-	let chargement =
-		`
+	return `
 	<div id="selected_article">
-				<h3 class="article-name">Nom du produit: <span>${donnees.name}</span></h3>
-				<h3 class="article-id">Id du produit: <span>${donnees._id}</span></h3>
-				<h3 class="article-description">Description du produit: <span>${donnees.description}</span></h3>
+				<h3 class="article-name">Nom du produit: <span>${parametre.name}</span></h3>
+				<h3 class="article-id">Id du produit: <span>${parametre._id}</span></h3>
+				<h3 class="article-description">Description du produit: <span>${parametre.description}</span></h3>
 				<form>
-					<label for="article-option"></label>
+					<label for="article-option">${parametre.option}</label>
 					<select name="article-option" id="article-option">
 					</select>
 				</form>
-				<h3 class="article-price">Prix: <span>${donnees.price}</span></h3>
-				<div class="${donnees.imageUrl}">
-						<img class="article-img" src="${donnees.imageUrl}" />
+				<h3 class="article-price">Prix: <span>${parametre.price}</span></h3>
+				<div class="${parametre.imageUrl}">
+						<img class="article-img" src="${parametre.imageUrl}" />
 				</div>
 			</div>
 			<button id="btnByeArt" type="submit" name="btnByeArt">Acheter</button>
-	`
-;
+	`;
 }
+
+
 
 //------------------------------------------------------------------------------------------
 
 
-/*
-const positionProduit = document.getElementById("selected_article");
-console.log(positionProduit);
-*/
-
-// injection HTML
-
-positionProduit.innerHTML = structureProduit;
