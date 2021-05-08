df6aaf5751c7648a418a372cee7a5e1b751ced35
Guerman <guerman.rouban@gmail.com>
2021-05-08 09:09:24 +0200

    Mise en place LS page produit

--- js/frontend/main/produits.js
+++ js/frontend/main/produits.js
@@ -1,3 +1,5 @@
+//-----------------------------------------------------------------------------------------------
+// ETAPE 1 - RECUPERATION DE L'ID
 
 const queryString_url_id = window.location.search;
 console.log(queryString_url_id);
@@ -9,14 +11,8 @@ const id = urlParams.get('id');
 console.log("id=", id);
 
 //-----------------------------------------------------------------------------------------------
+// ETAPE 2 - API REQUEST
 
-
-const positionProduit = document.getElementById("selected_article");
-//console.log(positionProduit);
-
-let donnees = [];
-
-// ETAPE 1 - API REQUEST
 const fetchProduit = async () => {
 	donnees = await fetch(`https://teddies-api.herokuapp.com/api/teddies/${id}`)
 		.then(res => res.json());
@@ -26,57 +22,85 @@ const fetchProduit = async () => {
 
 };
 
-
 //fetchProduit(); // ok on a bien recuperé l API
 const showProduit = async () => {
 	await fetchProduit();
 }
 showProduit();
-//-------------------------------------------------------
-// créer une fonction qui va générer l'ensemble des options, avec ces dernières passées en paramètre
-function showOptions(options){
-	console.log(options)// déclarer une variable vide qui va contenir le code HTML
-	let varHTMLvide = ``;
-	// parcourir les couleurs (les options passées en paramètre) via une boucle
-	for (let i = 0; i < options.length; i++) {
-		let option = + options[i];
-		console.log(options[i]);
-		let htmlOption = `<option value="${options[i]}">${options[i]}</option>`;
-		console.log(htmlOption);
-		varHTMLvide += htmlOption
-	}
-	return varHTMLvide;
 
-}
+//-----------------------------------------------------------------------------------------------
+// ETAPE 3 - LE CHARGEMENT DU HTML
+
 function structureProduit(parametre) {
 	let colors = showOptions(parametre.colors);
 	return `
 	<div id="selected_article">
-				<h3 class="article-name">Nom du produit: <span>${parametre.name}</span></h3>
-				<h3 class="article-id">Id du produit: <span>${parametre._id}</span></h3>
-				<h3 class="article-description">Description du produit: <span>${parametre.description}</span></h3>
+				<h3 class="article_name" id="article_name">Nom du produit: <span>${parametre.name}</span></h3>
+				<h3 class="article_id" id="article_id">Id du produit: <span>${parametre._id}</span></h3>
+				<h3 class="article_description" id="article_description">Description du produit: <span>${parametre.description}</span></h3>
 				<form>
-					<label for="article-option">Choisir la couleur</label>
-					<select name="article-option" id="article-option">
+					<label for="article_option">Choisir la couleur</label>
+					<select name="article_option" id="article_option">
 					${colors}
 					</select>
 				</form>
-				<h3 class="article-price">Prix: <span>${numberWhitComa(parametre.price)}</span></h3>
+				<h3 class="article_price" id="article_price">Prix: <span>${numberWhitComa(parametre.price)}</span></h3>
 				<div class="${parametre.imageUrl}">
-						<img class="article-img" src="${parametre.imageUrl}" />
+						<img class="article_img" id="article_img" src="${parametre.imageUrl}" />
 				</div>
 			</div>
-			<button id="btnByeArt" type="submit" name="btnByeArt">Acheter</button>
 	`
 }
+const positionProduit = document.getElementById("selected_article");
+//console.log(positionProduit);
 
+//-----------------------------------------------------------------------------------------------
+// ETAPE 4 - LA SELECTE BOX
+// créer une fonction qui va générer l'ensemble des options, avec ces dernières passées en paramètre
 
+function showOptions(options) {
+	//console.log(options)// déclarer une variable vide qui va contenir le code HTML
+	let varHTMLvide = ``;
+	// parcourir les couleurs (les options passées en paramètre) via une boucle
+	for (let i = 0; i < options.length; i++) {
+		let option = + options[i];
+		//console.log(options[i]);
+		let htmlOption = `<option value="${options[i]}">${options[i]}</option>`;
+		//console.log(htmlOption);
+		varHTMLvide += htmlOption
+	}
+	return varHTMLvide;
+}
+
+//-----------------------------------------------------------------------------------------------
+// ETAPE 5 - AFFICHAGE DU PRIX
 // Affichage correct du prix avec la virgule
 numberWhitComa = (x) => {
 	return x.toString().replace(/\B(?=(\d{2})+(?!\d))/g, ',') + " EUR TTC"
 };
 
+//-----------------------------------------------------------------------------------------------
+// ETAPE 5 - AFFICHAGE DU PRIX
+//localStorage.setItem("clé","valeur")
+//localStorage.getItem("clé")
+//localStorage.clear();
+//JSON.stringify(object); // transforme un objet en string
+//JSON.parse(string); // a l'inverse transforme un string en objet
 
-//------------------------------------------------------------------------------------------
+const local = JSON.parse(localStorage.getItem("cardSelected"));
+
+//if (localStorage.getItem("Nom") != null)//si la clé existe
 
 
+btnByeArt.onclick = () => {
+	const cardSelected = {
+		Nom:article_name,
+		Id: article_id,
+		Description: article_description,
+		Option: article_option.value,
+		Prix: article_price,
+		Photo: article_img
+	}
+	localStorage.setItem("card", JSON.stringify(cardSelected));
+	document.location.reload();
+}
---
+++ js/frontend/main/tools.js
@@ -0,0 +1,5 @@
+/*
+function cl(a) {
+	console.log(a)
+}
+*/ 
